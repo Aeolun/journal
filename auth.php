c660c4ee0090651ed9c64fcc894a8b558120ace6
Bart Riepe <aeolun@gmail.com>
2017-05-31 15:16:13 +0200

    Request access again if authentication details change

--- auth.php
+++ auth.php
@@ -9,6 +9,8 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {
     exit;
 } else {
     if ($user != $_SERVER['PHP_AUTH_USER'] or $password != $_SERVER['PHP_AUTH_PW']) {
+        header('WWW-Authenticate: Basic realm="Journal"');
+        header('HTTP/1.0 401 Unauthorized');
         echo 'You are not authorized to access this page';
         exit;
     }
